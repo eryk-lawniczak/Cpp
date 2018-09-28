93d5b9d7140d55929621b21f511d1c11780cc120
eryk-lawniczak <35136216+eryk-lawniczak@users.noreply.github.com>
2018-09-28 09:45:16 +0200

    Add files via upload

--- 3_pole_ograniczone_funkcja.cpp
+++ 3_pole_ograniczone_funkcja.cpp
@@ -2,31 +2,64 @@
 #include<cmath>
 using namespace std;
 
-double f(int x){
+double f(double x){
 
-    return (x*x) - x - 3;
-}
-double pole(double a, double b, double h){
-
-    return ((a+b)/2)*h;
+    double y = pow(x,2) - x -3;
+    return y;
 }
 
-int main (){
-    int p=3,
-        q=5,
-        n=10;
-    double dl = (q-p)/n;
-    double wynik;
 
 
-    for(int i=1; i<=n; i++){
-       if(i<n){
-         wynik = wynik + pole(f(p), f(p+dl), dl);
+double trapez(double p, double q, double dl){
+    double wynik =0;
 
-       }else
-       wynik = wynik + pole(f(p), f(q), dl);
-        p=p+dl;
+    for(double i=p; i<(q-dl); i+=dl){
+       wynik += (((f(i)+f(i+dl))*dl)/2);
+
     }
-    cout<<"Wynik to: "<<wynik;
+        return wynik;
+    }
+
+double pr_nadm(double p, double q, double dl){
+    double pole;
+     for(double i=p; i<(q-dl); i+=dl){
+       if(f(i)>f(i+dl)) pole += f(i)*dl;
+       else  pole += f(i+dl)*dl;
+
+    }
+    return pole;
+
+}
+
+double pr_podm(double p, double q, double dl){
+    double pole;
+     for(double i=p; i<(q-dl); i+=dl){
+       if(f(i)<f(i+dl)) pole += f(i)*dl;
+       else  pole += f(i+dl)*dl;
+
+    }
+    return pole;
+
+}
+double prost(double p, double q, double dl){
+    double pole;
+     for(double i=p; i<(q-dl); i+=dl){
+
+        pole += f(i+(dl/2))*dl;
+
+    }
+    return pole;
+
+}
+int main (){
+    double  p=3, q=5, n=20, dl = (q-p)/n;
+
+    cout<<"Trapez: "<<trapez(p, q, dl)<<endl;
+    cout<<"Prostokat z nadmiarem: "<<pr_nadm(p, q, dl)<<endl;
+    cout<<"Prostokat z niedomiarem: "<<pr_podm(p, q, dl)<<endl;
+    cout<<"Prostokat: "<<prost(p, q, dl)<<endl;
+
+
+
     return 0;
 }
